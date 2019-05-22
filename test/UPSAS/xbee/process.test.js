22c4375dcfc5ba6ba488b8b906f2ac55fe813658
장혁 <hyuk.jang@smsoft.co.kr>
2019-05-22 18:09:12 +0900

    @ 자동 프로세스 T.C 정리 중

--- test/UPSAS/xbee/process.test.js
+++ test/UPSAS/xbee/process.test.js
@@ -172,14 +172,14 @@ describe.skip('Manual Mode', function() {
 describe('Automatic Mode', function() {
   this.timeout(10000);
 
-  /** @type {reqFlowCmdInfo} 저수조 > 증발지 1-A */
+  /** @type {reqFlowCmdInfo} 저수지 > 증발지 1-A */
   const rvToSEB1A = {
     srcPlaceId: 'RV',
     destPlaceId: 'SEB_1_A',
     wrapCmdType: reqWrapCmdType.CONTROL,
   };
 
-  /** @type {reqFlowCmdInfo} 저수조 > 증발지 1-B */
+  /** @type {reqFlowCmdInfo} 저수지 > 증발지 1-B */
   const rvToSEB1B = {
     srcPlaceId: 'RV',
     destPlaceId: 'SEB_1_B',
@@ -218,27 +218,27 @@ describe('Automatic Mode', function() {
    * @desc T.C 1 [자동 모드]
    * 다중 흐름 명령을 요청하고 이에 반하는 흐름 명령을 요청하여 충돌 체크가 제대로 동작하는지 확인
    * @description
-   * 1. 저수조 > 증발지 1-A 명령 요청.
+   * 1. 저수지 > 증발지 1-A 명령 요청.
    * trueNodeList: ['V_006', 'V_001', 'P_002'],
    * falseNodeList: ['GV_001'],
-   * 2. 저수조 > 증발지 1-B 명령 요청. 명령 충돌 발생 X
+   * 2. 저수지 > 증발지 1-B 명령 요청. 명령 충돌 발생 X
    * trueNodeList: ['V_006', 'V_002', 'P_002'],
    * falseNodeList: ['GV_002'],
    * 3. 증발지 1-A > 해주 1 명령 요청. 명령 충돌 발생 O
    * trueNodeList: ['GV_001', 'WD_013', 'WD_010'],
    * falseNodeList: ['WD_016'],
    * 4. 증발지 1-A > 해주 1 명령 취소. 존재하지 않으므로 X
-   * 5. 저수조 > 증발지 1-A 명령 취소.
+   * 5. 저수지 > 증발지 1-A 명령 취소.
    * 'V_001' 닫힘. 'GV_001' OC 해제
    * 6. 증발지 1-A > 해주 1 명령 요청. 명령 충돌 발생 X
-   * 7. 저수조 > 증발지 1-B 명령 취소.
+   * 7. 저수지 > 증발지 1-B 명령 취소.
    * 'P_002', 'V_002', 'V_006' 순으로 닫힘. OC 해제
    * 8. 증발지 1-A > 해주 1 명령 취소.
    * OC는 전부 해제, 존재 명령 X, 모든 장치는 닫힘
    */
   it('Multi Flow Command Control & Conflict & Cancel', async () => {
     // BU.CLI('Multi Flow Command Control & Conflict & Cancel');
-    // 1. 저수조 > 증발지 1-A 명령 요청. 펌프 2, 밸브 6, 밸브 1 . 실제 제어 true 확인 및 overlap 확인
+    // 1. 저수지 > 증발지 1-A 명령 요청. 펌프 2, 밸브 6, 밸브 1 . 실제 제어 true 확인 및 overlap 확인
     const cmdRvTo1A = control.executeFlowControl(rvToSEB1A);
 
     // 실제 True 장치 목록
@@ -267,7 +267,7 @@ describe('Automatic Mode', function() {
 
     // expect(realFalseCmdRvTo1A.).
 
-    // * 2. 저수조 > 증발지 1-B 명령 요청. 실제 제어 추가 확인 V_002
+    // * 2. 저수지 > 증발지 1-B 명령 요청. 실제 제어 추가 확인 V_002
     // * trueList: ['V_006', 'V_002', 'P_002'],
     // * falseList: ['GV_002'],
     // await Promise.delay(100);
@@ -292,11 +292,11 @@ describe('Automatic Mode', function() {
     // False O.C 는 2개, trueList: ['GV_001', 'GV_002'],
     expect(_.filter(existOverlapList, { singleControlType: FALSE })).to.length(2);
 
-    // 저수조 > 증발지 1-A 명령 완료.
+    // 저수지 > 증발지 1-A 명령 완료.
     const firstCompleteWCU = await eventToPromise(control, 'completeCommand');
     // expect(cmdRvTo1A.wrapCmdUUID).to.eq(firstCompleteWCU);
 
-    // 저수조 > 증발지 1-B 명령 완료.
+    // 저수지 > 증발지 1-B 명령 완료.
     const secondCompleteWCU = await eventToPromise(control, 'completeCommand');
     // expect(cmdRvTo1B.wrapCmdUUID).to.eq(secondCompleteWCU);
 
@@ -316,7 +316,7 @@ describe('Automatic Mode', function() {
       'The command(SEB_1_A_TO_BW_1) does not exist and you can not issue a CANCEL command.',
     );
 
-    // * 5. 저수조 > 증발지 1-A 명령 취소.
+    // * 5. 저수지 > 증발지 1-A 명령 취소.
     rvToSEB1A.wrapCmdType = reqWrapCmdType.CANCEL;
     const cancelCmdRvTo1A = control.executeFlowControl(rvToSEB1A);
 
@@ -375,7 +375,7 @@ describe('Automatic Mode', function() {
 
     await eventToPromise(control, 'completeCommand');
 
-    // * 7. 저수조 > 증발지 1-B 명령 취소.
+    // * 7. 저수지 > 증발지 1-B 명령 취소.
     // * 'P_002', 'V_002', 'V_006' 순으로 닫힘. OC 해제
     rvToSEB1B.wrapCmdType = reqWrapCmdType.CANCEL;
     const cancelCmdRvTo1B = control.executeFlowControl(rvToSEB1B);
@@ -436,12 +436,11 @@ describe('Automatic Mode', function() {
 
   /**
    * @desc T.C 2 [자동 모드]
-   * @desc 이 TC는 Echo Server와 함께 수행해야 한다.
    * 달성 목표가 있는 명령은 목표가 완료되었을 때 명령 스택에서 사라져야 한다.
    * @description
-   * 1. 저수조 > 증발지 1-A 명령 요청. 달성 목표: 수위 10cm [Echo]. 수위 조작 후 명령 삭제 확인.
-   * 2. 저수조 > 증발지 1-A 명령 요청. 달성 제한 시간: 2 Sec. 시간 초과 후 명령 삭제 확인.
-   * 3. 저수조 > 증발지 1-A 명령 요청. 달성 목표: 수위 10cm [Echo]. 제한시간: 2 Sec. 수위 조작 후 타이머 Clear 처리 및 명령 삭제 확인.
+   * 1. 저수지 > 증발지 1-A 명령 요청. 달성 목표: 수위 10cm. 수위 조작 후 명령 삭제 확인.
+   * 2. 저수지 > 증발지 1-A 명령 요청. 달성 제한 시간: 2 Sec. 시간 초과 후 명령 삭제 확인.
+   * 3. 저수지 > 증발지 1-A 명령 요청. 달성 목표: 수위 10cm. 제한시간: 2 Sec. 수위 조작 후 타이머 Clear 처리 및 명령 삭제 확인.
    */
   it('Critical Command ', async () => {
     // BU.CLI('Critical Command');
@@ -452,7 +451,7 @@ describe('Automatic Mode', function() {
 
     const { criticalManager } = control.model;
 
-    // * 1. 저수조 > 증발지 1-A 명령 요청. 달성 목표: 수위 10cm [Echo]. 수위 조작 후 명령 삭제 확인.
+    // * 1. 저수지 > 증발지 1-A 명령 요청. 달성 목표: 수위 10cm. 수위 조작 후 명령 삭제 확인.
     rvToSEB1A.wrapCmdType = reqWrapCmdType.CONTROL;
     rvToSEB1A.wrapCmdGoalInfo = {
       goalDataList: [
@@ -464,13 +463,13 @@ describe('Automatic Mode', function() {
       ],
     };
 
-    // 저수조 > 증발지 1-A 명령 요청
+    // 저수지 > 증발지 1-A 명령 요청
     let cmdRvTo1A = control.executeFlowControl(rvToSEB1A);
     // 명령이 완료되기를 기다림
     await eventToPromise(control, 'completeCommand');
     console.time('Step 1');
 
-    // 저수조 > 증발지 1-A 임계치 저장소 가져옴
+    // 저수지 > 증발지 1-A 임계치 저장소 가져옴
     let criStoRvTo1A = criticalManager.getCriticalComponent(cmdRvTo1A);
     let criGoalRvTo1A = criStoRvTo1A.getCriticalComponent(NODE_BT_001);
     // 새로운 임계치 명령이 등록되야함.
@@ -505,7 +504,7 @@ describe('Automatic Mode', function() {
 
     expect(control.model.complexCmdList).to.length(0);
 
-    // * 2. 저수조 > 증발지 1-A 명령 요청. 달성 제한 시간: 2 Sec. 시간 초과 후 명령 삭제 확인.
+    // * 2. 저수지 > 증발지 1-A 명령 요청. 달성 제한 시간: 2 Sec. 시간 초과 후 명령 삭제 확인.
     rvToSEB1A.wrapCmdGoalInfo = {
       limitTimeSec: 1,
       goalDataList: [
@@ -548,6 +547,32 @@ describe('Automatic Mode', function() {
     // 현재 누적 OC는 존재하지 않음
     expect(control.model.cmdManager.findExistOverlapControl()).to.length(0);
   });
+
+  /**
+   * @desc T.C 3 [자동 모드]
+   * 해주 및 증발지의 면적에 따른 해수 부피를 산정하여 명령 수행 가능성 여부를 결정한다.
+   * @description
+   * 1. Map에 설정되어 있는 모든 장소의 임계치를 등록하고 초기화 한다.
+   * 2. 수위: 5cm, 염도: (수중 증발지: 3도, 일반 증발지 4: 10도), 모듈 온도: 30도, 해주 및 저수지 수위: 1m로 설정한다.
+   * 3. 해주 1 수위를 하한선(10cm)으로 설정. [해주 1 > 증발지 1-A] 명령 요청. 실패 X
+   * 4. 해주 2 수위를 상한선(100cm)으로 설정. [증발지 1-A > 해주 2 ] 명령 요청. 실패 X
+   * 5. 해주 1 수위를 적정선(70cm)으로 설정. [해주 1 > 증발지 1-A] 명령 요청. 성공 O
+   * 6. 해주 2 수위를 하한선(10cm)으로 설정. [증발지 1-A > 해주 2] 명령 요청. 성공 O
+   */
+
+  /**
+   * @desc T.C 4 [자동 모드]
+   * 장소 임계치 동작을 검증한다.
+   * @description
+   * 1. Map에 설정되어 있는 모든 장소 임계치를 등록하고 초기화 한다.
+   * 2. 수위: 5cm, 염도: (수중 증발지: 3도, 일반 증발지 4: 10도), 모듈 온도: 30도, 해주 및 저수지 수위: 1m로 설정한다.
+   * 3. 1초 대기 후 WL_001의 수위를 하한 수위 1cm로 조작한다. 장소 임계치 설정에 의한 [저수지 > 증발지 1-A] 명령 요청
+   * 4. 1초 대기 후 WL_002의 수위를 상한 수위 11cm로 조작한다. 장소 임계치 설정에 의한 [증발지 1-B > 해주 1] 명령 요청
+   * 5. 1초 대기 후 
+   * 5. 1초 대기 후 MRT_003의 온도를 50도로 올리고
+   * 5. 1초 대기 후 WL_001의 수위를 상한 수위 7cm로 조작한다. 임계치 명령 달성 조건 충족으로 [저수지 > 증발지 1-A] 명령 취소
+   * 6. 1초 대기 후 WL_002의 수위를 적정 수위 5cm로 조작한다. 임계치 명령 달성 조건 충족으로 [증발지 1-B > 해주 1] 명령 취소
+   */
 });
 
 // const Converter = require('device-protocol-converter-jh');
